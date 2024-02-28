ed44e5c81a96aca5271ee9dd91e77e589ef80da5
omi-here2 <omiiiitk@gmail.com>
2024-02-28 17:49:50 +0530

    update

--- src/components/Common/Navbar.jsx
+++ src/components/Common/Navbar.jsx
@@ -37,13 +37,13 @@ function Navbar() {
   // console.log("sub links", subLinks)
 
   const matchRoute = (route) => {
-    return matchPath({ path: route }, location.pathname)
+    return matchPath({ path: route }, location?.pathname)
   }
 
   return (
     <div
       className={`flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700 ${
-        location.pathname !== "/" ? "bg-richblack-800" : ""
+        location?.pathname !== "/" ? "bg-richblack-800" : ""
       } transition-all duration-200`}
     >
       <div className="flex w-11/12 max-w-maxContent items-center justify-between">
@@ -56,7 +56,7 @@ function Navbar() {
           <ul className="flex gap-x-6 text-richblack-25">
             {NavbarLinks?.map((link, index) => (
               <li key={index}>
-                {link.title === "Catalog" ? (
+                {link?.title === "Catalog" ? (
                   <>
                     <div
                       className={`group relative flex cursor-pointer items-center gap-1 ${
@@ -65,13 +65,13 @@ function Navbar() {
                           : "text-richblack-25"
                       }`}
                     >
-                      <p>{link.title}</p>
+                      <p>{link?.title}</p>
                       <BsChevronDown />
                       <div className="invisible absolute left-[50%] top-[50%] z-[1000] flex w-[200px] translate-x-[-50%] translate-y-[3em] flex-col rounded-lg bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all duration-150 group-hover:visible group-hover:translate-y-[1.65em] group-hover:opacity-100 lg:w-[300px]">
                         <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-richblack-5"></div>
                         {loading ? (
                           <p className="text-center">Loading...</p>
-                        ) : subLinks.length ? (
+                        ) : subLinks?.length ? (
                           <>
                             {subLinks
                               ?.filter(
@@ -79,14 +79,14 @@ function Navbar() {
                               )
                               ?.map((subLink, i) => (
                                 <Link
-                                  to={`/catalog/${subLink.name
+                                  to={`/catalog/${subLink?.name
                                     .split(" ")
                                     .join("-")
                                     .toLowerCase()}`}
                                   className="rounded-lg bg-transparent py-4 pl-4 hover:bg-richblack-50"
                                   key={i}
                                 >
-                                  <p>{subLink.name}</p>
+                                  <p>{subLink?.name}</p>
                                 </Link>
                               ))}
                           </>
@@ -105,7 +105,7 @@ function Navbar() {
                           : "text-richblack-25"
                       }`}
                     >
-                      {link.title}
+                      {link?.title}
                     </p>
                   </Link>
                 )}
